a533399d9443073eb3e174e4da607258c946a78c
bprlab <bprlab@10.200.144.32>
2018-03-24 14:23:47 +0300

    Test Massege

---
+++ MavenHesaplamalarr/src/main/java/Hesaplamar/Hesaplamalar.java
@@ -0,0 +1,18 @@
+package Hesaplamar;
+
+public class Hesaplamalar {
+	public static void main(String[]args) {
+		int a,b;
+		int toplam=0;
+		
+		
+		a=5;
+		b=10;
+		toplam=a+b;
+		
+		System.out.println(toplam);
+		
+		
+	}
+
+}
